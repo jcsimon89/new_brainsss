327bed505718bac52b120a74387bbca4890b2f63
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2024-04-22 11:47:54 -0700

    Update stitch.sh

--- scripts/stitch.sh
+++ scripts/stitch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=stitch_nii
-#SBATCH --partition=owners
+#SBATCH --partition=trc
 #SBATCH --time=2-00:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=48 
+#SBATCH --cpus-per-task=24
 #SBATCH --output=./logs/stitchlog3.out
 #SBATCH --open-mode=append
 #SBATCH --mail-type=ALL
