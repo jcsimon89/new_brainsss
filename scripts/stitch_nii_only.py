64954cddd1aed0e354467d035173826b8a38113f
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2024-06-26 13:56:09 -0700

    update path

--- scripts/stitch_nii_only.py
+++ scripts/stitch_nii_only.py
@@ -13,12 +13,12 @@ import gc
 
 #get to files
 #date = '20211215'
-dates = ['20240412']  #as of 4-27 4-5 still has one bad fly as does 330
+dates = ['20240501','20240507','20240514','20240516','20240517', '20240524','20240531']  #as of 4-27 4-5 still has one bad fly as does 330
 for date in dates:
   print('STARTING DATE:', str(date))
   #dataset_path = "/oak/stanford/groups/trc/data/Ashley2/imports/" + str(date)
   #dataset_path = "/oak/stanford/groups/trc/data/krave/bruker_data/imports/" + str(date)
-  dataset_path = f"/oak/stanford/groups/trc/data/Jacob/ImagingData/Bruker/{date}"
+  dataset_path = f"/oak/stanford/groups/trc/data/Jacob/Bruker/{date}"
   
   fly_files = os.listdir(dataset_path)  ## find directory names, they are the fly names
   fly_folders = []
